c7787564ac412e315162b62f8266c4c6807ca16d
ankit <rathaurankit501@gmail.com>
2026-08-17 23:12:56 +0530

    add getUserChannelProfile controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -169,7 +169,7 @@ const resetPassword = asyncHandler(async (req, res) => {
 });
 
 const getCurrentUser = asyncHandler(async (req, res) => {
-  res
+  return res
     .status(200)
     .json(new ApiResponse(200, "user fetched successfully", req.user));
 });
@@ -195,6 +195,67 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, "avatar updated successfully", response));
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+  if (!username?.trim()) throw new ApiError(400, "username is missing ");
+
+  // aggregate returns the array of objects
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribersTo",
+      },
+    },
+    {
+      subscribersCount: {
+        $size: "$subscribers",
+      },
+      channelSubscribedToCount: {
+        $size: "$subscribersTo",
+      },
+      isSubscribed: {
+        $cond: {
+          if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+          then: true,
+          else: false,
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        email: 1,
+        avatar: 1,
+        coverImage: 1,
+        subscribersCount: 1,
+        channelSubscribedToCount: 1,
+        isSubscribed: 1,
+      },
+    },
+  ]);
+  if (!channel?.length) throw new ApiError(400, "channel doesn't exist");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "user fetched successfully "));
+});
+
 export {
   registerUser,
   loginUser,
@@ -203,4 +264,5 @@ export {
   resetPassword,
   getCurrentUser,
   updateUserAvatar,
+  getUserChannelProfile,
 };
